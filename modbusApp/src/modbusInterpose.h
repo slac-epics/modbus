8b573932528a7192308ba63ff8c61dcd4f468fe9
Bruce Hill <bhill@slac.stanford.edu>
2010-10-25 00:14:33 +0000

    Enumeration values set to constants and install modbusInterpose.h

--- modbusApp/src/modbusInterpose.h
+++ modbusApp/src/modbusInterpose.h
@@ -11,9 +11,9 @@
 #include <shareLib.h>
 
 typedef enum {
-    modbusLinkTCP,
-    modbusLinkRTU,
-    modbusLinkASCII
+    modbusLinkTCP=0,
+    modbusLinkRTU=1,
+    modbusLinkASCII=2
 } modbusLinkType;
 
 #ifdef __cplusplus
