e2ddc0ada7e75bebd6cdbd91e9a7c5832f260940
no author <noauthor@slac.stanford.edu>
2014-10-06 17:10:02 +0000

    This commit was manufactured by cvs2svn to create tag 'modbus-R2-7_0-1'.
    Sprout from Sheng 2012-06-25 15:21:44 UTC Ernest Williams <ernesto@slac.stanford.edu> 'initial import of modbus modbusR2-4 from ANL'
    Cherrypick from master 2014-10-06 17:10:01 UTC J. Mitch D'Ewart <mdewart@slac.stanford.edu> 'updated RELEASE_NOTES for modbus-R2-7_0-1':
        RELEASE_NOTES
        RELEASE_SITE
        configure/RELEASE
        documentation/BK9000andBK9050andBK9100.pdf
        documentation/modbus.html
        documentation/modbusDoc.html
        documentation/modbusReleaseNotes.html
        modbusApp/Db/statistics.template
        modbusApp/src/Makefile
        modbusApp/src/drvModbusAsyn.c
        modbusApp/src/modbus.h
        modbusApp/src/modbusInterpose.c
        modbusApp/src/testModbusSyncIO.cpp
    Cherrypick from master 2014-10-06 16:29:11 UTC J. Mitch D'Ewart <mdewart@slac.stanford.edu> 'Added INT32_LE_ALT and INT32_BE_ALT to support alternative data format':
        modbusApp/src/drvModbusAsyn.h
    Delete:
        .cvsignore
        configure/RELEASE.win32-x86
        configure/RELEASE.win32-x86-debug
        iocBoot/iocTest/.cvsignore
        modbusApp/op/adl/.cvsignore

--- modbusApp/src/drvModbusAsyn.h
+++ modbusApp/src/drvModbusAsyn.h
@@ -14,25 +14,31 @@
  */
 
 #define MODBUS_DATA_STRING             "MODBUS_DATA" 
+#define MODBUS_READ_STRING             "MODBUS_READ" 
 #define MODBUS_UINT16_STRING           "UINT16" 
 #define MODBUS_INT16_SM_STRING         "INT16SM" 
 #define MODBUS_BCD_UNSIGNED_STRING     "BCD_UNSIGNED" 
 #define MODBUS_BCD_SIGNED_STRING       "BCD_SIGNED" 
 #define MODBUS_INT16_STRING            "INT16" 
 #define MODBUS_INT32_LE_STRING         "INT32_LE" 
+#define MODBUS_INT32_LE_ALT_STRING     "INT32_LE_ALT"
 #define MODBUS_INT32_BE_STRING         "INT32_BE" 
+#define MODBUS_INT32_BE_ALT_STRING     "INT32_BE_ALT"
 #define MODBUS_FLOAT32_LE_STRING       "FLOAT32_LE" 
 #define MODBUS_FLOAT32_BE_STRING       "FLOAT32_BE" 
 #define MODBUS_FLOAT64_LE_STRING       "FLOAT64_LE" 
 #define MODBUS_FLOAT64_BE_STRING       "FLOAT64_BE" 
 #define MODBUS_ENABLE_HISTOGRAM_STRING "ENABLE_HISTOGRAM"
 #define MODBUS_READ_HISTOGRAM_STRING   "READ_HISTOGRAM"
+#define MODBUS_HISTOGRAM_BIN_TIME_STRING  "HISTOGRAM_BIN_TIME"
+#define MODBUS_HISTOGRAM_TIME_AXIS_STRING "HISTOGRAM_TIME_AXIS"
 #define MODBUS_POLL_DELAY_STRING       "POLL_DELAY"
 #define MODBUS_READ_OK_STRING          "READ_OK"
 #define MODBUS_WRITE_OK_STRING         "WRITE_OK"
 #define MODBUS_IO_ERRORS_STRING        "IO_ERRORS"
 #define MODBUS_LAST_IO_TIME_STRING     "LAST_IO_TIME"
 #define MODBUS_MAX_IO_TIME_STRING      "MAX_IO_TIME"
+
 
 typedef enum {
     dataTypeUInt16,           /* 16-bit unsigned               drvUser=UINT16 */
@@ -41,7 +47,9 @@ typedef enum {
     dataTypeBCDSigned,        /* 16-bit signed BCD             drvUser=BCD_UNSIGNED */
     dataTypeInt16,            /* 16-bit 2's complement         drvUser=INT16 */
     dataTypeInt32LE,          /* 32-bit integer little-endian  drvUser=INT32_LE */
+    dataTypeInt32LEAlt,       /* 32-bit integer little-endian, alternate data format  drvUser=INT32_LE_ALT */
     dataTypeInt32BE,          /* 32-bit integer big-endian     drvUser=INT32_BE */
+    dataTypeInt32BEAlt,       /* 32-bit integer big-endian, alternate data format     drvUser=INT32_BE_ALT */
     dataTypeFloat32LE,        /* 32-bit float little-endian    drvuser=FLOAT32_LE */
     dataTypeFloat32BE,        /* 32-bit float big-endian       drvUser=FLOAT32_BE */
     dataTypeFloat64LE,        /* 64-bit float little-endian    drvuser=FLOAT64_LE */
